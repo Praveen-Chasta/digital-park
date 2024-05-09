ebd9b4b1b058790d7d252ed78e36eb38e4fecc1f
Ashish yadav <ashish.yadav@pleximus.in>
2024-05-09 19:06:36 +0530

    dashboard code remove

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './components/layouts/Main.jsx';
 import About from './components/About.jsx';
 import ClassComponent from './components/ClassComponent.jsx';
 import {BrowserRouter as Router ,Routes ,Route} from "react-router-dom";
-import Dashboard from './components/backend/dashboard.jsx';
 
 function App() {
     return ( 
@@ -13,7 +12,6 @@ function App() {
                     <Route path="/" element={<Home/>}></Route> 
                     <Route path="/about"  element={<About/>}/>
                     <Route path="/classes"  element={<ClassComponent/>}/>
-                    <Route path="/dashboard"  element={<Dashboard/>}/>
                     
                 </Routes> 
         </Router>  
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,13 +1,12 @@
 import React,{ useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 import logo from "../../asset/images/digital-edu-park-logo.webp"
-import { useNavigate } from 'react-router-dom';
- const Footer =()=>{
+const Footer =()=>{
 	const [showButton, setShowButton] = useState(false);
 	const navigate = useNavigate();
 		const onSubmit=()=>
 		{
-			navigate('/dashboard');
+			navigate('/');
 		}
 
 	useEffect(() => {
