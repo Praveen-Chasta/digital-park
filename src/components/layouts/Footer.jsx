5b4f122932b4373f4361842bdbddf7a9c203b4f9
Ashish yadav <ashish.yadav@pleximus.in>
2024-05-09 19:13:14 +0530

    redirect changes done

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,6 +1,7 @@
 import React,{ useState, useEffect } from "react";
 import {Link} from "react-router-dom";
-import logo from "../../asset/images/digital-edu-park-logo.webp"
+import logo from "../../asset/images/digital-edu-park-logo.webp";
+import { useNavigate } from 'react-router-dom';
 const Footer =()=>{
 	const [showButton, setShowButton] = useState(false);
 	const navigate = useNavigate();
