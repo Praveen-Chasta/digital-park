2de23b1f9a7ae72de3cf4178d31033048998d565
Ashish yadav <ashish.yadav@pleximus.in>
2024-05-31 17:40:19 +0530

    data table in result page

--- src/admin/exam/Result.jsx
+++ src/admin/exam/Result.jsx
@@ -4,14 +4,19 @@ import Header from "../layouts/Header/Header.jsx";
 import Footer from "../layouts/Footer/Footer.jsx";
 import Sidebar from "../layouts/Sidebar/Sidebar.jsx";
 import dashboardStyle from "../dashboard/dashboard.module.css"
+import ResultDatatable from "./resultDatatable.jsx";
 
 import { useSelector } from "react-redux";
 
 function Result() {
 
   const quizResult = useSelector((state) => state.quizQuestions.quizResult) || [];
-
-
+  if (quizResult.quiz_id) 
+    {
+       localStorage.setItem('result_quiz_id', quizResult.quiz_id);
+     }
+ 
+ 
   return (
     <>
     <div className={dashboardStyle['main-wrapper']}>
@@ -20,10 +25,13 @@ function Result() {
       <div className={dashboardStyle['page-wrapper']}>
         <div className="container">
         <div className="section mt-5">
-              <div className="question-info mt-5">
+          
+              <div className="question-info1 mt-5">
               <h2>Quiz Result</h2>
               <p>Total No of Questions : {quizResult.no_of_question}</p>
               <p>Your score: {quizResult.total_getting_mark} out of {quizResult.total_marks}</p>
+                <ResultDatatable/>
+             
           </div>
           </div>
       </div>
---
+++ src/admin/exam/resultDatatable.jsx
@@ -0,0 +1,106 @@
+import DataTable from "react-data-table-component";
+import React, { useState, useCallback, useEffect } from "react";
+import axios from "axios";
+import { SERVER_URL } from "../../config";
+import "./ExamPage.css";
+
+const ResultDatatable=()=>{
+  const[search,setSearch] = useState("");
+  const[test ,setTest] = useState(false);
+  const [questionPoint,setQuestionPoint] = useState([]); 
+  const [filterOption,setfilterOption] = useState([]); 
+  
+  
+const BASE_URL = SERVER_URL;
+
+const CustomOptionCell = ({ row }) => (
+   
+  <div>
+      
+      <input type="radio" name={`option_${row.result_id}`} checked={row.correct_answer === row.answer}   readOnly/> {row.option1} 
+      <input type="radio" name={`option_${row.result_id}`} value={row.option2}  checked={row.correct_answer === row.answer}  style={{ marginLeft: "25px"  }} readOnly/> {row.option2} 
+      <input type="radio" name={`option_${row.result_id}`} value={row.option3}  checked={row.correct_answer === row.answer}  style={{  marginLeft:"25px"}}readOnly /> {row.option3} 
+      <input type="radio" name={`option_${row.result_id}`} value={row.option4}  checked={row.correct_answer === row.answer} style={{ marginLeft: "25px" }} readOnly/> {row.option4}
+  </div>
+);
+
+  const  getResult = async()=>{
+    try
+    {
+      const token = await localStorage.getItem('token');
+      const quiz_id = await localStorage.getItem('result_quiz_id');
+      
+      const response = await axios.post(`${BASE_URL}/all-question-result`,
+      {
+        quiz_id:quiz_id,
+      },
+      {
+        headers:
+         {
+          'remember-token': token,
+        }
+      }
+    )
+     setQuestionPoint(response.data.data);
+      setfilterOption(response.data.data);
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+ }
+
+  const columns = [
+    {
+      name: 'Question',
+      selector: row => row.question,
+      sortable:true,
+      style:  { minWidth: '300px'},
+      
+      wrap: true },
+    {
+      name: 'Your Choice Answer',
+      style:  { minWidth: '500px' } ,
+      cell: row => <CustomOptionCell row={row} />,
+      wrap: true
+    },
+    
+    {
+      name: 'Marks',
+      selector: row => row.mark,
+     },
+  ];
+  
+  useEffect(()=>{
+    getResult();
+  },[]);
+  useEffect(()=>{
+     const result=questionPoint.filter((item)=>
+        {
+             return item.question.toLowerCase().match(search.toLowerCase());
+         }); 
+
+      setfilterOption(result);  
+      
+  },[search]);
+  
+   return(
+    <>
+    <div className="datatable-container">
+      <DataTable columns={columns}
+                data={filterOption}
+                title="Result"
+                fixedHeader
+                pagination
+                paginationPerPage={5}
+                highlightOnHover  
+                subHeader    
+                subHeaderComponent={
+                  <input type="text"  placeholder="Search here" className="form-control w-25"  value={search} onChange={(e)=>setSearch(e.target.value)}/>
+                }    
+               
+                />
+      </div>   
+    </>);
+}
+export default ResultDatatable;
