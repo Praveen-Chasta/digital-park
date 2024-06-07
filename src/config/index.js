ef05076fca947a6dec44b0184d9ea2ed207719d4
Ashish yadav <ashish.yadav@pleximus.in>
2024-06-07 17:33:25 +0530

    url change

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
- export const SERVER_URL = "http://127.0.0.1:8000/api/edu";
-//  export const SERVER_URL = "https://digitaledupark.pleximus.co.in/api/edu";
+//  export const SERVER_URL = "http://127.0.0.1:8000/api/edu";
+ export const SERVER_URL = "https://digitaledupark.pleximus.co.in/api/edu";
